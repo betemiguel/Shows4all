a9c36682ff4a0b1db13119a3a9393eef39d163f0
Elisabete Miguel <betemiguel29@gmail.com>
2021-05-08 15:51:25 +0100

    Add Series to Pages

---
+++ Shows4all/Shows4all.App/Pages/Series/Delete.cshtml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Shows4all.App.Data.Context;
+using Shows4all.App.Data.Entities;
+
+namespace Shows4all.App.Pages.Series
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Shows4all.App.Data.Context.Shows4AllDbContext _context;
+
+        public DeleteModel(Shows4all.App.Data.Context.Shows4AllDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Serie Serie { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Serie = await _context.Series
+                .Include(s => s.Season).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Serie == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Serie = await _context.Series.FindAsync(id);
+
+            if (Serie != null)
+            {
+                _context.Series.Remove(Serie);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
