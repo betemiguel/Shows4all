2dbdff44a27e365319c247d77a364ecd17e4351e
Elisabete Miguel <betemiguel29@gmail.com>
2021-05-08 15:42:14 +0100

    Add Rentals to Pages

---
+++ Shows4all/Shows4all.App/Pages/Rentals/Delete.cshtml.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using Shows4all.App.Data.Context;
+using Shows4all.App.Data.Entities;
+
+namespace Shows4all.App.Pages.Rentals
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly Shows4all.App.Data.Context.Shows4AllDbContext _context;
+
+        public DeleteModel(Shows4all.App.Data.Context.Shows4AllDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Rental Rental { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Rental = await _context.Rentals
+                .Include(r => r.Customer)
+                .Include(r => r.Serie).FirstOrDefaultAsync(m => m.Id == id);
+
+            if (Rental == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Rental = await _context.Rentals.FindAsync(id);
+
+            if (Rental != null)
+            {
+                _context.Rentals.Remove(Rental);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
